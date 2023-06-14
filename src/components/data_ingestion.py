bfcd8a2ece328e8856172c9a3cdf8505bca5d04d
Ashish <mandalk092@gmail.com>
2023-06-14 21:26:04 +0530

    data_ingestion change

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -20,26 +20,26 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv("notebook\data\stud.csv")
+            df=pd.read_csv("notebook\data\HeartDisease.csv")
             logging.info("Read the dataset as dataframe")
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_congif.train_data_path),exist_ok=True)
 
-            df.to_csV(self.ingestion_config.raw_data_path, index=False,header=True)
+            df.to_csv(self.ingestion_congif.raw_data_path, index=False,header=True)
 
             logging.info("Train test split initiated")
             train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
             
-            train_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            train_set.to_csv(self.ingestion_congif.train_data_path,index=False,header=True)
 
-            test_set.to_csv(self.ingestion_config_test_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_congif.test_data_path,index=False,header=True)
 
             logging.info("Ingestion of the data is completed")
 
 
             return(
-                self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path
+                self.ingestion_congif.train_data_path,
+                self.ingestion_congif.test_data_path
                 )
 
         except Exception as e:
@@ -47,6 +47,6 @@ class DataIngestion:
 
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     obj = DataIngestion()
     obj.initiate_data_ingestion()
